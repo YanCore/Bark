8fed8eb5b41049c236b195897cb19a4aff264882
YanCore <zzxnormal@163.com>
2024-08-03 01:13:20 +0800

    更新

--- src/query.py
+++ src/query.py
@@ -20,11 +20,20 @@ def query(query: str) -> ResultResponse:
 
     if token:
         # bark = BarkNotificator(device_token=token)
-        title, content = parsed_query
-        # bark.send(title=title, content=content)
-        # bark
+        if len(parsed_query) == 2:
+            title, content = parsed_query
+            # bark.send(title=title, content=content)
+            # bark
+            api_url = 'https://api.day.app/{key}/{message}'
+            url = api_url.format(key=token, message=f"{title}:{content}")
+            result = Result(
+                Title=title,
+                SubTitle=content,
+                JsonRPCAction=open_url(url)
+            )
+            return send_results(result)
         api_url = 'https://api.day.app/{key}/{message}'
-        url = api_url.format(key=token, message=f"{title}:{content}")
+        url = api_url.format(key=token, message=query)
         result = Result(
             Title=title,
             SubTitle=content,
